5bb7f7dad2a03d9fc84d814316f7b61eac858d33
eneskarateke <eneskarateke@gmail.com>
2023-04-19 00:12:18 +0300

    s5g2 projesi tamamlandı

--- src/index.js
+++ src/index.js
@@ -7,3 +7,68 @@ import './less/index.less'
 
 
 // Kodlar buraya gelecek!
+
+// document.querySelector("h1").addEventListener("click", e => {
+//     alert("Bana tıkladın!")
+//     });
+
+  const anasayfaLink = document.querySelectorAll('.nav-link');
+  anasayfaLink.forEach(link => {
+    link.addEventListener('dblclick', (event) => {
+    event.preventDefault();
+    window.open(link.href, '_blank');
+  });})
+
+
+
+  anasayfaLink.forEach((link) => {
+    link.addEventListener("mouseover", () => {
+      link.style.fontWeight = "bold"; 
+    });
+    link.addEventListener("mouseout", () => {
+      link.style.fontWeight = "normal"; });
+  });
+
+  
+
+const resimler = document.getElementsByTagName("img");
+
+for (let i = 0; i < resimler.length; i++) {
+  resimler[i].addEventListener("mouseenter", event => {
+    const img = event.target;
+    img.style.transition = "1s";
+    img.style.transform = "scale(1.3)";
+  });
+
+  resimler[i].addEventListener("mouseleave", event => {
+    const img = event.target;
+    img.style.transform = "scale(1)";
+      });
+    }
+
+
+
+
+const butonlar = document.querySelectorAll(".btn");
+
+butonlar.forEach(btn => {   
+btn.addEventListener("click", event => {
+  alert("Seni almamı istediğime emin misin???!")
+  })
+
+btn.addEventListener("mouseenter", event =>{
+  event.target.style.background ="yellow";
+  event.target.style.color ="black";
+} )
+
+btn.addEventListener("mouseleave", event =>{
+  event.target.style.background ="";
+  event.target.style.color ="";
+} )
+
+});
+
+
+
+
+
